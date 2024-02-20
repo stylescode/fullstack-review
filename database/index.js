bcf533584e9c4bcabf1547f34dd5e2f01b9790d2
Styles <stylesk11@gmail.com>
2024-02-19 20:29:06 -0800

    write schema

--- database/index.js
+++ database/index.js
@@ -3,11 +3,20 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  id: Number,
+  repo_name: String,
+  link: String,
+  description: String,
+  owner: String,
+  stars: Number,
+  watchers: Number,
+  forks: Number,
+
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (userInfo) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
