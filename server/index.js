8341398127152a3496f6d5232943d3f73c3cf9e6
Styles <stylesk11@gmail.com>
2024-02-19 20:28:09 -0800

    fill out post method

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const github = require('../helpers/github.js');
+
+
+
 let app = express();
+
+app.use(express.json());
+
+
 
 // TODO - your code here!
 // Set up static file service for files in the `client/dist` directory.
@@ -11,11 +19,32 @@ app.post('/repos', function (req, res) {
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+
+  console.log('on server side');
+
+  console.log(req.body);
+
+  let userName = req.body.name;
+
+  github.getReposByUsername(userName, (error, result) => {
+    if (error) {
+      callback(error);
+    } else {
+      console.log('back in server/index.js');
+      console.log(result);
+      return;
+    }
+  })
+
+  // then put result into database
+
+
 });
 
 app.get('/repos', function (req, res) {
   // TODO - your code here!
   // This route should send back the top 25 repos
+  console.log(req.body);
 });
 
 let port = 1128;
