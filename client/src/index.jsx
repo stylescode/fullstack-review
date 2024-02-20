a017723cbde61d3c134b0f0f9971d24781497a1b
Styles <stylesk11@gmail.com>
2024-02-19 20:29:26 -0800

    add post request method to search

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,12 +4,25 @@ import $ from 'jquery';
 import Search from './components/Search.jsx';
 import RepoList from './components/RepoList.jsx';
 
+import axios from 'axios';
+
 const App = () => {
 
   const [repos, setRepos] = useState([]);
 
+  const Server = 'http://localhost:1128/repos';
+
   const search = (term) => {
     console.log(`${term} was searched`);
+    // should send POST request
+    axios.post(Server, {term})
+      .then((response) => {
+        // response should be an updated list of repos in database
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
   }
 
   return (
