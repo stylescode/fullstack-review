778efc806fe3f894fd4307039e12d178d1c01199
Styles <stylesk11@gmail.com>
2024-02-19 20:27:25 -0800

    add get method to retrieve repo data

--- helpers/github.js
+++ helpers/github.js
@@ -1,19 +1,35 @@
 const axios = require('axios');
 const config = require('../config.js');
 
-let getReposByUsername = (/* TODO */) => {
+let getReposByUsername = (username) => {
   // TODO - Use the axios module to request repos for a specific
   // user from the github API
 
-  // The options object has been provided to help you out, 
+  // The options object has been provided to help you out,
   // but you'll have to fill in the URL
+
+  console.log('getting user info from github...');
+
+
   let options = {
-    url: 'FILL ME IN',
+    url: `https://api.github.com/users/${username}/repos`,
     headers: {
-      'User-Agent': 'request',
+      'User-Agent': 'stylescode',
       'Authorization': `token ${config.TOKEN}`
     }
   };
+
+  console.log('getting part 2');
+
+  axios.get(options.url, {headers: options.headers})
+    .then((response) => {
+      console.log('user info retrieved');
+      return (JSON.stringify(response));
+    })
+    .catch((error) => {
+      console.log('could not retrieve user info');
+      return error;
+    })
 
 }
 
